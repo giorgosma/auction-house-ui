e1c7fa844128caddc9f100cccec58b55c995c6c2
stathismast <sdi1500092@di.uoa.gr>
2019-08-23 21:06:21 +0300

    Added basic login using the API

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,21 +1,52 @@
 <template>
   <div>
     Login.vue here
+    <el-input v-model="username" placeholder="Username"></el-input>
+    <el-input v-model="password" placeholder="Password"></el-input>
     <el-button @click="handleLogin">Click here to login</el-button>
+    <p v-if="showWarning">Invalid username or password</p>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: 'login',
   data() {
     return {
-      global: this.$store.state
+      global: this.$store.state,
+
+      username: "",
+      password: "",
+
+      showWarning: false,
     }
   },
   methods:{
-    handleLogin(){
-      this.global.loggedIn = true
+    async handleLogin(){
+      if(this.username == "" || this.password == ""){
+        this.showWarning = true
+        return
+      }
+
+      var url = this.global.apiurl
+              + "users/login/"
+              + this.username
+              + "/"
+              + this.password
+
+      var response = await axios.post(url)
+
+      console.log(JSON.stringify(response.data))
+
+      if(response.data == "OK"){
+        this.global.loggedIn = true
+      }
+      else{
+        this.showWarning = true
+      }
+
     }
   }
 }
--- src/store.js
+++ src/store.js
@@ -5,6 +5,8 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    apiurl: "http://83.212.109.75:28992/",
+
     loggedIn: false,
   },
   mutations: {
