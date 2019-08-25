6c84d8cb76884f5592f7dbfdaa764da9152e047e
unknown <sdi1500089@di.uoa.gr>
2019-08-25 11:42:22 +0300

    Added User Register

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,45 +1,55 @@
 <template>
   <div>
     <el-row>
-      <el-col class="column" :span="12">
+      <el-col class="column" :span="13">
         <el-form>
-          <el-form-item label="Item Name" label-width="120px">
-            <el-input v-model="item.name"></el-input>
+          <el-form-item label="Email" label-width="140px">
+            <el-input placeholder="name@email.com" v-model="user.email" clearable></el-input>
           </el-form-item>
-          <el-form-item label="Description" label-width="120px">
-            <el-input
-              type="textarea"
-              :rows="4"
-              placeholder="Item Description"
-              v-model="item.description"
-            ></el-input>
+          <el-form-item label="Username" label-width="140px">
+            <el-input placeholder="Please input username" v-model="user.username" clearable></el-input>
           </el-form-item>
-          <el-form-item label="Starting Price" label-width="120px">
-            <el-input-number v-model="auction.starting_bid" :precision="2" :step="0.01" :min="0"></el-input-number>
+          <el-form-item label="Password" label-width="140px">
+            <el-input placeholder="Please input password" v-model="user.password" show-password></el-input>
           </el-form-item>
-          <el-form-item label="Buy Out Price" label-width="120px">
-            <el-input-number v-model="auction.buyout_price" :precision="2" :step="0.01" :min="0"></el-input-number>
+          <el-form-item label="Confirm Password" label-width="140px">
+            <el-input placeholder="Please input password" v-model="confirmedPassword" show-password></el-input>
           </el-form-item>
-          <el-form-item label="End Time" label-width="120px">
-            <el-date-picker
-              type="date"
-              placeholder="Pick a date"
-              v-model="auction.end_time"
-              style="width: 100%;"
-            ></el-date-picker>
-            <el-time-picker
-              placeholder="Pick a time"
-              v-model="auction.end_time"
-              style="width: 100%;"
-            ></el-time-picker>
+          <el-form-item label="First Name" label-width="140px">
+            <el-input placeholder="Please input first name" v-model="user.first_name" clearable></el-input>
+          </el-form-item>
+          <el-form-item label="Last Name" label-width="140px">
+            <el-input placeholder="Please input last name" v-model="user.last_name" clearable></el-input>
+          </el-form-item>
+          <el-form-item label="City" label-width="140px">
+            <el-input placeholder="Please input city" v-model="user.city" clearable></el-input>
+          </el-form-item>
+          <el-form-item label="Country" label-width="140px">
+            <el-input placeholder="Please input country" v-model="user.country" clearable></el-input>
+          </el-form-item>
+          <el-form-item label="Telephone" label-width="140px">
+            <el-input placeholder="Please input telephone" v-model="user.telephone" clearable></el-input>
+          </el-form-item>
+          <el-form-item label="AFM" label-width="140px">
+            <el-input placeholder="Please input afm" v-model="user.afm" clearable></el-input>
           </el-form-item>
         </el-form>
+      </el-col>
+      <el-col class="column" :offset="1" :span="3">
+        <div v-if="user.password">
+          <p v-if="showWarning">
+            <el-alert title="Password Validation" type="success" :closable="false" show-icon></el-alert>
+          </p>
+          <p v-else-if="!showWarning">
+            <el-alert title="Error Password Validation" type="error" :closable="false" show-icon></el-alert>
+          </p>
+        </div>
       </el-col>
     </el-row>
 
     <el-row>
-      <el-col :offset="9">
-        <el-button type="primary" @click="handleNewAuction">Create Auction</el-button>
+      <el-col :offset="10">
+        <el-button type="primary" @click="handleNewRegistration">Confirm Registration</el-button>
       </el-col>
     </el-row>
   </div>
@@ -47,22 +57,32 @@
 
 <script>
 export default {
-  name: "newauction",
   data() {
     return {
       global: this.$store.state,
 
-      item: {
-        name: "",
-        description: ""
+      user: {
+        email: "",
+        username: "",
+        password: "",
+        first_name: "",
+        last_name: "",
+        city: "",
+        country: "",
+        telephone: "",
+        afm: ""
       },
 
-      auction: {
-        starting_bid: 0,
-        buyout_price: 0,
-        end_time: null,
-      }
+      confirmedPassword: "",
+      userPassword: "",
+
+      showWarning: false,
+      typedPassword: false
     };
+  },
+  watch: {
+    confirmedPassword: "checkingPassword",
+    "user.password": "checkingPassword"
   },
   mounted() {
     this.updateBreadcrumb();
@@ -71,39 +91,38 @@ export default {
     updateBreadcrumb() {
       this.global.breadcrumbPath = [
         { path: "/", name: "Home" },
-        { path: "/newAuction", name: "New Auction" }
+        { path: "/register", name: "Register" }
       ];
     },
-    async handleNewAuction(){
-      console.log("This is handle new auction")
-      console.log(JSON.stringify(this.item))
-      console.log(JSON.stringify(this.auction))
-
-      var url = this.global.apiurl
-              + "items/newItem"
-      var body = this.item
-      var config = { headers: { 'Content-Type': 'application/json' } }
-
-      var response = await axios.post(url, body, config)
-
-      console.log(response.data)
-
-      this.auction.item_id = response.data.id
-      this.auction.seller_id = this.global.userInfo.id
-
-      if(this.auction.buyout_price == 0){
-        delete this.auction.buyout_price
+    checkingPassword() {
+      console.log("checking passwords");
+      if (this.confirmedPassword != "") {
+        if (this.user.password == this.confirmedPassword) {
+          this.showWarning = true;
+        } else {
+          this.showWarning = false;
+        }
       }
+    },
+    emptyPassword() {
+      console.log("empty passwords");
+      if (this.user.password != "") {
+        this.typedPassword = true;
+      } else {
+        this.typedPassword = false;
+      }
+    },
+    async handleNewRegistration() {
+      console.log("This is handle new Registration");
+      console.log(JSON.stringify(this.user));
 
-      console.log(JSON.stringify(this.auction))
+      var url = this.global.apiurl + "users/register";
+      var body = this.user;
+      var config = { headers: { "Content-Type": "application/json" } };
 
-      url = this.global.apiurl
-            + "auctions/newAuction"
-      body = this.auction
+      var response = await axios.post(url, body, config);
 
-      response = await axios.post(url, body, config)
-
-      console.log(response.data)
+      console.log(response.data);
     }
   }
 };
