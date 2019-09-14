8b7fef2306231ee9b03de574a12d0b94166914ec
unknown <sdi1500089@di.uoa.gr>
2019-09-14 11:58:14 +0300

    Added UI for Search Auctions

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,35 @@
 <template>
   <div>
-    <el-row>Choose Categories
+    <el-row>
+      <el-col :span="2">
+        <el-select v-model="select" placeholder="Select">
+          <el-option label="Category" value="Category"></el-option>
+          <el-option label="Price" value="Price"></el-option>
+          <el-option label="Text" value="Text"></el-option>
+          <el-option label="Country" value="Country"></el-option>
+        </el-select>
+      </el-col>
+      <el-col :span="6">
+        <div v-if="select == 'Price'" :span="16">
+          <el-row :span="6">
+          <el-input-number placeholder="Please input min" :precision="2" :step="0.01" :min="0" v-model="min" class="input-with-select">
+          </el-input-number>
+          </el-row>
+          <el-row :span="6">
+          <el-input-number placeholder="Please input max" :precision="2" :step="0.01" :min="0" v-model="max" class="input-with-select">
+          </el-input-number>
+          </el-row> 
+          <el-button @click="searchViaPrice" slot="append" icon="el-icon-search"></el-button>          
+        </div>
+        <div v-else>
+          <el-input placeholder="Please input" v-model="input" class="input-with-select">
+            <el-button @click="searchVia" slot="append" icon="el-icon-search"></el-button>
+          </el-input>
+        </div>
+      </el-col>
+    </el-row>
+    <el-row>
+      Choose Categories
       <el-select
         v-model="value"
         multiple
@@ -67,7 +96,10 @@ export default {
       auctionNumber: 0,
       options: [],
       value: [],
-
+      select: "",
+      input: "",
+      max: null,
+      min: 0
     };
   },
   watch: {
@@ -92,25 +124,23 @@ export default {
       this.auctionNumber = response.data.length;
       this.auctions = response.data;
       for (let i in this.auctions) {
-        this.auctions[i].remaining = this.getRemainingTime(this.auctions[i].end_time)
+        this.auctions[i].remaining = this.getRemainingTime(
+          this.auctions[i].end_time
+        );
         if (this.auctions[i].remaining == "Expired")
-          this.auctions[i].visible = false
+          this.auctions[i].visible = false;
         else {
           if (this.global.loggedIn) {
-            if (this.global.userInfo.id == this.auctions[i].seller_id){
-              this.auctions[i].visible = false
-            }
-            else
-              this.auctions[i].visible = true
-          }         
-          else
-            this.auctions[i].visible = true
+            if (this.global.userInfo.id == this.auctions[i].seller_id) {
+              this.auctions[i].visible = false;
+            } else this.auctions[i].visible = true;
+          } else this.auctions[i].visible = true;
         }
       }
-      this.global.auctions = this.auctions
+      this.global.auctions = this.auctions;
     },
     loadViaCategory() {
-      console.log("Changed value!")
+      console.log("Changed value!");
       if (this.value.length == 0) {
         for (let i in this.auctions) {
           this.auctions[i].visible = true;
@@ -118,34 +148,32 @@ export default {
         return;
       }
 
-      var flag = false
+      var flag = false;
       for (let i in this.auctions) {
-        this.auctions[i].visible = false
+        this.auctions[i].visible = false;
         for (let j in this.value) {
           for (let k in this.auctions[i].item.categories) {
             if (this.auctions[i].item.categories[k].id == this.value[j]) {
-              this.auctions[i].visible = true
+              this.auctions[i].visible = true;
               flag = true;
               break;
             }
           }
           if (flag) {
-            flag = false
+            flag = false;
             break;
           }
         }
         if (this.auctions[i].visible == true) {
-          console.log(JSON.stringify(this.auctions[i]))
+          console.log(JSON.stringify(this.auctions[i]));
         }
       }
-
     },
-    async loadCategories(){
-      var url = this.global.apiurl
-              + "categories/all"
+    async loadCategories() {
+      var url = this.global.apiurl + "categories/all";
 
-      var response = await axios.get(url)
-      this.global.categories = response.data
+      var response = await axios.get(url);
+      this.global.categories = response.data;
       for (let i in this.global.categories) {
         this.options.push({
           value: this.global.categories[i].id,
@@ -153,27 +181,87 @@ export default {
         });
       }
     },
-    getRemainingTime(end_time){
-      var seconds = (new Date(end_time) - this.calcTime(6))/1000
-      if(seconds < 0) return "Expired"
+    getRemainingTime(end_time) {
+      var seconds = (new Date(end_time) - this.calcTime(6)) / 1000;
+      if (seconds < 0) return "Expired";
 
-      var sec_num = parseInt(seconds)
-      var days    = Math.floor(sec_num / 86400)
-      var hours   = Math.floor((sec_num - (days * 86400)) / 3600);
-      var minutes = Math.floor((sec_num - (days * 86400) - (hours * 3600)) / 60);
-      var seconds = sec_num - (days * 86400) - (hours * 3600) - (minutes * 60);
+      var sec_num = parseInt(seconds);
+      var days = Math.floor(sec_num / 86400);
+      var hours = Math.floor((sec_num - days * 86400) / 3600);
+      var minutes = Math.floor((sec_num - days * 86400 - hours * 3600) / 60);
+      var seconds = sec_num - days * 86400 - hours * 3600 - minutes * 60;
 
-      if (days    < 10) {days    = "0"+days;}
-      if (hours   < 10) {hours   = "0"+hours;}
-      if (minutes < 10) {minutes = "0"+minutes;}
-      if (seconds < 10) {seconds = "0"+seconds;}
-      return days+'d '+hours+'h '+minutes+'m';
+      if (days < 10) {
+        days = "0" + days;
+      }
+      if (hours < 10) {
+        hours = "0" + hours;
+      }
+      if (minutes < 10) {
+        minutes = "0" + minutes;
+      }
+      if (seconds < 10) {
+        seconds = "0" + seconds;
+      }
+      return days + "d " + hours + "h " + minutes + "m";
     },
     calcTime(offset) {
       var d = new Date();
       var utc = d.getTime() + d.getTimezoneOffset() * 60000;
       return new Date(utc + 3600000 * offset);
     },
+    searchViaPrice(){
+      console.log(this.select)
+      if (this.min == 0 && this.max == 0) {
+        this.loadAllAuctions()
+        return
+      }
+      this.callSearch("auctions/searchAuctionViaPrice/" + this.min + "/" + this.max)
+    },
+    searchVia(){
+      console.log(this.select)
+      if (this.input == ""){
+        this.loadAllAuctions()
+        return;
+      }
+      if(this.select == "Category"){
+        this.callSearch("auctions/searchAuctionViaCategory/" + this.input)
+      }
+      else if(this.select == "Country"){
+        this.callSearch("auctions/searchAuctionViaCountry/" + this.input)
+      }
+      else if(this.select == "Text") {
+        this.callSearch("auctions/searchAuctionViaFreeText/" + this.input)
+      }
+    },
+    async callSearch(param){
+      console.log(param)
+
+      var url = this.global.apiurl + param;
+
+      var response = await axios.get(url);
+      // console.log(JSON.stringify(response.data))
+      // console.log(response.data.length);
+      this.auctionNumber = response.data.length;
+      this.auctions = response.data;
+
+      for (let i in this.auctions) {
+        this.auctions[i].visible = false;
+        this.auctions[i].remaining = this.getRemainingTime(this.auctions[i].end_time);
+        if (this.auctions[i].remaining == "Expired")
+          this.auctions[i].visible = false;
+        else {
+          if (this.global.loggedIn) {
+            if (this.global.userInfo.id == this.auctions[i].seller_id) {
+              this.auctions[i].visible = false;
+            } else this.auctions[i].visible = true;
+          } 
+          else this.auctions[i].visible = true;
+          console.log(JSON.stringify(this.auctions[i]))
+        }
+      }
+      this.global.auctions = this.auctions;
+    }
   }
 };
 </script>
@@ -209,5 +297,12 @@ export default {
 
 .clearfix:after {
   clear: both;
+}
+
+.el-select .el-input {
+  width: 110px;
+}
+.input-with-select .el-input-group__prepend {
+  background-color: #fff;
 }
 </style>
